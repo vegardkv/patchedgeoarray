aec1b24248a0a85b8cba8d96031b92fb9b602584
vegardkv <vkvernelv@gmail.com>
2022-01-02 20:15:13 +0100

    Remove PatchedGeoArray class

--- patchedgeoarray/__init__.py
+++
@@ -1,4 +0,0 @@
-from .patchedgeoarray import (PatchedGeoArray, NdArrayPatchedGeoArray, FileBasedPatchedGeoArray, BoundingBox,
-                              OutOfBoundsError)
-
-__all__ = ['PatchedGeoArray', 'NdArrayPatchedGeoArray', 'FileBasedPatchedGeoArray', 'BoundingBox', 'OutOfBoundsError']
